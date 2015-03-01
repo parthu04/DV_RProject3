fe67de310a939c4c3b9f9ebc417db6b029aaf7ed
Parth Patel <parthu04@gmail.com>
2015-03-01 16:31:17 -0600

    updates

---
+++ 01 Data/Providers dataset.R
@@ -0,0 +1,1 @@
+pdf <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from PROVIDERS"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_php274', PASS='orcl_php274', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
