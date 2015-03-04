0532918ede7fd43ba59d320a4561a012ac1476fc
Ana Phan <ana.phan@utexas.edu>
2015-03-03 21:29:06 -0600

    Group_by

--- 02 Data Wrangling/data wrangling 1.R
+++ 02 Data Wrangling/data wrangling 1.R
@@ -1,1 +1,2 @@
-data_wrange_vis1 <- mjdf %>% group_by(STATE) %>% summarise(mean(COMPUTERS.MATH.AND.STATS))
+data_wrange_vis1 <- mjdf %>% group_by(STATE) %>% summarise(mean_compsci = mean(COMPUTERS.MATH.AND.STATS), n=n()) %>% tbl_df
+head(data_wrange_vis1)
