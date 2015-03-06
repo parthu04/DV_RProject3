8eddf0c8cbbec8d9b8eae342db39342030f5d46d
Ana Phan <ana.phan@utexas.edu>
2015-03-06 11:04:52 -0600

    Edited .Rmd

--- 02 Data Wrangling/data wrangling 1.R
+++ 02 Data Wrangling/data wrangling 1.R
@@ -1,15 +1,12 @@
 data_wrangle1 <- mjdf %>% group_by(STATE) %>% summarise(COMPUTERS.MATH.AND.STATS = mean(COMPUTERS.MATH.AND.STATS), BIOLOGY.AND.AGRICULTURE = mean(BIOLOGY.AND.AGRICULTURE), PHYSICS = mean(PHYSICS), PSYCHOLOGY = mean(PSYCHOLOGY), SOCIOLOGY = mean(SOCIOLOGY), ENGINEERING = mean(ENGINEERING), MULTIDISCIPLINARY.STUDIES = mean(MULTIDISCIPLINARY.STUDIES), SCIENCES = mean(SCIENCES), BUSINESS = mean(BUSINESS), EDUCATION = mean(EDUCATION), LITERATURE.AND.LANGUAGES = mean(LITERATURE.AND.LANGUAGES), LIBERAL.ARTS.AND.HISTORY = mean(LIBERAL.ARTS.AND.HISTORY), VISUAL.AND.PERFORMING.ARTS = mean(VISUAL.AND.PERFORMING.ARTS), COMMUNICATION = mean(COMMUNICATION), OTHER = mean(OTHER))
 
 
-tidy_wrangle1 <- gather(data_wrangle1, Degree, 'STATE')
-names(tidy_wrangle1) <- c("State", "Degree", "Percentage")
-
-states <- c('AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'KS', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY')
-
 l <- list()
-for (i in states) {
-  newdata <- subset(tidy_wrangle1, State == i)
-  p <- ggplot(newdata, aes(x=Degree, y=Percentage)) + geom_bar(stat='identity') +ggtitle(i) + coord_flip()
+for (i in range(53)) {
+  row  <- data_wrangle1[i ,c('STATE', 'COMPUTERS.MATH.AND.STATS', 'PHYSICS', 'PSYCHOLOGY', 'SOCIOLOGY', 'ENGINEERING', 'MULTIDISCIPLINARY.STUDIES', 'SCIENCES', 'BUSINESS', 'EDUCATION', 'LITERATURE.AND.LANGUAGES', 'LIBERAL.ARTS.AND.HISTORY', 'VISUAL.AND.PERFORMING.ARTS', 'COMMUNICATION', 'OTHER')]
+  values <- row[,c('COMPUTERS.MATH.AND.STATS', 'PHYSICS', 'PSYCHOLOGY', 'SOCIOLOGY', 'ENGINEERING', 'MULTIDISCIPLINARY.STUDIES', 'SCIENCES', 'BUSINESS', 'EDUCATION', 'LITERATURE.AND.LANGUAGES', 'LIBERAL.ARTS.AND.HISTORY', 'VISUAL.AND.PERFORMING.ARTS', 'COMMUNICATION', 'OTHER')]
+  lbls <- names(values)
+  p <- pie(as.numeric(values), labels = lbls , main=row[1])
   print(p)
   l[[i]] <- p
 }
--- 02 Data Wrangling/data wrangling 2.R
+++ 02 Data Wrangling/data wrangling 2.R
@@ -1,3 +1,3 @@
 data_wrangle2 <- sigdf %>% group_by(State) %>% summarise(Total.Graduates = mean(Total.Graduates), Average.Income = mean(Average.Income)) %>% subset(!State == "null")
-  
-ggplot(data_wrangle2, aes(x=Total.Graduates, y=Average.Income)) + geom_point(aes(colour=State)) + guides(col=guide_legend(nrow=20))
+
+ggplot(data_wrangle2, aes(x=Total.Graduates, y=Average.Income)) + geom_point(aes(colour=State)) + guides(col=guide_legend(nrow=20)) + ggtitle('Total Graduates and the Average Income of States') + theme(plot.title=element_text(size=20, face="bold", vjust=2)) + labs(x="Total Graduates", y=paste("Average", "Income")) 
